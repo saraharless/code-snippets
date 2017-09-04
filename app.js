46b8953bc247027baaf7a3db9e7ab721214d4559
saraharless <sara.e.harless@gmail.com>
2017-09-04 15:59:59 -0500

    add list of todos

--- app.js
+++ app.js
@@ -29,4 +29,19 @@ app.listen(3000, function() {
 
 
 //////////////////////////////////////////////////////
-To Do:
+// To Do:
+// a title
+// a body (the code)
+// optional notes
+// a language
+// tags -- that is, user-defined words or phrases that classify the code, like "authentication", "front-end", "middleware", or "database".
+// Your application must:
+//
+// Tests are optional.
+//
+// have registration and login
+// allow you to create a snippet
+// allow you to see a list of all your snippets
+// allow you to see a list of all your snippets for a specific language
+// allow you to see a list of all your snippets for a specific tag
+// allow you to look at an individual snippet
