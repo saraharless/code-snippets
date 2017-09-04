9abdc3bcd50ab51bbe25d262bd3dbc7c74b73158
saraharless <sara.e.harless@gmail.com>
2017-09-04 15:39:28 -0500

    input html into index page, added consts and listen

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Snippet = require('./models/snippet.js');
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log('you did it, or something');
+})
