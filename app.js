dd3e56ddebd8ea104938adc2abc3c5fc19d91695
saraharless <sara.e.harless@gmail.com>
2017-09-04 16:12:15 -0500

    connect local host to index with no errors

--- app.js
+++ app.js
@@ -5,19 +5,70 @@ const mongoose = require('mongoose');
 const Snippet = require('./models/snippet.js');
 
 mongoose.Promise = require('bluebird');
-let url = 'mongodb://localhost:27017/list';
-mongoose.connect(url);
-const ObjectId = require('mongodb').ObjectID;
+mongoose.connect('mongodb://localhost:27017/snippets');
+
+const app = express();
 app.engine('mustache', mustacheExpress());
-app.set('views', './views')
-app.set('view engine', 'mustache')
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
-
+app.get('/', function (req, res) {
+  res.render('index');
+})
+// app.get('/snippets', function(req, res) {
+//   models.User.findAll()
+//     .then(function(users) {
+//       res.render('users', {
+//         userskey: users
+//       })
+//     })
+// })
+// app.get('/new_user', function(req, res) {
+//   res.render('new_user')
+// })
+// app.get('/update/:id', function(req, res) {
+//   models.User.findById(req.params.id)
+//     .then(function(users) {
+//       res.render('update', {userskey: users})
+//       // console.log(user);
+//     })
+// })
+//
+// app.post('/create_user', function(req, res) {
+//   const userToCreate = models.User.build({
+//     name: req.body.name,
+//     email: req.body.email,
+//     bio: req.body.bio
+//   });
+//   userToCreate.save().then(function() {
+//     res.redirect('/users');
+//   })
+// })
+//
+// app.post('/delete_user/:idOfTheUser', function(req, res) {
+//   // console.log('the id is: ' + req.params.idOfTheUser);
+//   models.User.destroy({
+//       where: {
+//         id: req.params.idOfTheUser
+//       }
+//     })
+//     .then(function() {
+//       res.redirect('/users')
+//     })
+// })
+//
+// app.post('/update/:idOfTheUser', function(req, res) {
+//   const userToUpdate = models.User.update({
+//     name: req.body.name,
+//     email: req.body.email,
+//     bio: req.body.bio
+//   },{where: {id: req.params.idOfTheUser}})
+//     .then(function() {
+//       res.redirect('/users')
+//     })
+// })
 
 
 
